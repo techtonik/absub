a93bc59d1cc0ae3c4899d37caeb0819352de3248
anatoly techtonik <techtonik@gmail.com>
2012-12-20 09:36:15 +0300

    Add test.

---
+++ async_test.py
@@ -0,0 +1,49 @@
+# Licensed to the public domain
+# by anatoly techtonik <techtonik@gmail.com>
+
+import os
+import sys
+import tempfile
+import time
+
+print("""\
+The test should run for 5 seconds, showing
+one line per second. If you see all five
+lines at once, that means test fails.
+""")
+
+
+longscript = """\
+import time
+
+for x in range(5):
+  print(x)
+  time.sleep(1)
+"""
+(fd, abspath) = tempfile.mkstemp('.async.py')
+os.write(fd, longscript)
+os.close(fd)
+
+python = sys.executable
+cmd = [python, abspath]
+
+
+
+from async_subprocess import AsyncPopen, PIPE
+# this returns immediately leaving child process
+# executed in background
+#p = AsyncPopen(cmd)
+p = AsyncPopen(cmd, stdout=PIPE)
+
+counter = 0
+while p.poll() is None:
+  time.sleep(0.001) # sleep to avoid 100% CPU load
+  counter += 1
+  out, err = p.communicate()
+  if out:
+    print("Got '%s' on %dth iteration." % (out, counter))
+
+# there is no cleanup, because os.remove() fires up
+# faster than Python from Popen() gets to the script
+# file
+os.remove(abspath)
